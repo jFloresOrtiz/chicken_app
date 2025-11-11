bac3fd6ec464a090ddefe45c5a8ea4f058d3eea5
jFloresOrtiz <johanroot19@gmail.com>
2025-11-10 22:06:27 -0500

    correcto cors

--- src/main/java/com/chicken/inventario/Config/CorsConfig.java
+++ src/main/java/com/chicken/inventario/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // tu origen local
+                        .allowedOrigins("*", "http://127.0.0.1:5501") // tu origen local
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
